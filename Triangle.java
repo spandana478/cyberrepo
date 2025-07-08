42e278d1a2b5144fc9a27626a1e5b2a45c0371bb
spandana <rspandana611@gmail.com>
2025-07-08 14:52:08 +0530

    java program

---
+++ Triangle.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+class Triangle
+{
+public static void main(String args[])
+{
+Scanner s = new Scanner(System.in);
+double base = 5.5;
+double height = 10;
+double area;
+area = 0.5 * base * height;
+System.out.println("The area of triangle is= "+area);
+}
+}
